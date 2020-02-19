cfa288be64b642a9e0fae9b2cd96ca125c5990e6
Rose <rosemschneider@gmail.com>
2020-02-18 16:12:37 -0800

    RMS changes and code review

--- reanalysis/0-clean.R
+++ reanalysis/0-clean.R
@@ -2,9 +2,16 @@ rm(list = ls())
 library(tidyverse)
 library(magrittr)
 
+##adding tidylog for transparency
+library(tidylog)
+
 # Read in data ====
 # original data
 full.data <- read.csv('raw data/recursion_full.csv', na.strings=c(""," ","NA", "NA "))
+
+# Exclusions ====
+
+## Removing individual participants
 # final decision: remove kids who fail 1 trial out of 3 trials, with no additional information about whether E reran the failed trial. Removing 8 kids in total
 # added 033516-ML on 3018-09-03 to the exclusion list. kid had NA data for wcn practice trials and 0 correct on test. assume kid failed wcn.
 full.data %<>%
@@ -14,24 +21,32 @@ full.data %<>%
                                    LadlabID == "012316-BO"| LadlabID == "041316-NC"|
                                    LadlabID == "022516-ML", "fail wcn", levels(ExclusionGroup)[ExclusionGroup]),
          ExclusionGroup = as.factor(ExclusionGroup))
+
+##RMS: I think we can get rid of below
 full.data %>%
   filter(LadlabID == "022616-JM")
+
+## Exclusion ns and reasons
 # Subject counts
 table(distinct(full.data, LadlabID, ExclusionGroup)$ExclusionGroup)
+
+## Filter out exclusions
 # Final data set
 full.data %<>%
   dplyr::filter(ExclusionGroup == "include")
 
 # Highest Count Coding ====
-# productivity, fhc, ihc coding from pc, jc, and rms
+## productivity, fhc, ihc coding from pc, jc, and rms
 
-# Read in data and keep final coding decisions
+## Read in data and keep final coding decisions
 hc.data <- read.csv('raw data/recursion_hc-coding-wide.csv') %>%
   dplyr::select(LadlabID, prod_tomerge, ihc_final, fhc_final, dce, why_productive, suptimes.final=suptimes_final,
                 ProductivityStrict)
-# add to full dataframe
+
+## add to full dataframe
 full.data <- dplyr::left_join(full.data, hc.data, by = "LadlabID")
-# Rename variables and factor levels
+
+## Rename variables and factor levels
 full.data %<>%
   dplyr::rename(Productivity = prod_tomerge, 
                 IHC = ihc_final, 
@@ -51,20 +66,21 @@ full.data %<>%
          ProductivityStrict.tertiary = as.factor(ProductivityStrict.tertiary),
          AgeMonths = floor(Age*12))
 
-# do a quick check to make sure we have the same SIDs in each
+## do a quick check to make sure we have the same SIDs in each
 unique.full <- as.vector(unique(full.data$LadlabID))
 unique.hc <- as.vector(unique(hc.data$LadlabID))
 tmp <- hc.data %>%
   dplyr::filter(LadlabID %in% unique.full)
-# good to go - the only kids who are not included in full data are the ones who we excluded
+### good to go - the only kids who are not included in full data are the ones who we excluded
 subjectlist <- unique(full.data$LadlabID)
 
-# Add in coding for reminder prompts and recovery from reminders
+## Add in coding for reminder prompts and recovery from reminders
 reminders.data <- read.csv('raw data/recursion_hc-reminders-wide.csv') %>%
   dplyr::select(LadlabID, reminders.total, reminders.recovered)
 full.data <- dplyr::left_join(full.data, reminders.data, by="LadlabID")
 
-# Add in productivity gradient calculation
+##RMS: We don't use productivity gradient anymore, right? So we can get rid of below
+## Add in productivity gradient calculation
 full.data %<>%
   mutate(delta.hc = FHC-IHC, 
          prod.gradient = case_when(
@@ -72,26 +88,26 @@ full.data %<>%
            IHC <99 & FHC>=100 ~ 1,
            IHC <99 & FHC<100 ~delta.hc/(99-IHC)))
 
-# Read in data file with counting errors individually labeled
-# This is wide format separate long data frame for highst count errors, for analysis and Fig 2
+## Read in data file with counting errors individually labeled
+### This is wide format separate long data frame for highst count errors, for analysis and Fig 2
 hc.errorscoded <- read.csv('raw data/recursion_hc-errors_long.csv', stringsAsFactors = F)
 hc.errorscoded %<>% filter(LadlabID %in% subjectlist)
 
-# Replace subject IDs
+### Replace subject IDs
 hc.errorscoded %<>% mutate(SortedID = fct_reorder(LadlabID, prod_tomerge, min)) %>%
   mutate(SortedID = fct_reorder(SortedID, fhc, min)) %>%
   mutate(SortedID = fct_reorder(SortedID, dce, min)) %>%
   mutate(SortedID = fct_reorder(SortedID, ihc, min)) %>%
   mutate(subID = factor(as.numeric(SortedID), ordered = F))
 
-# Add subject IDs to full.data
+## Add subject IDs to full.data
 full.data %<>% left_join(dplyr::select(hc.errorscoded, LadlabID, subID), by='LadlabID') %>%
   unique()
 
 ## Next Number ----
 wcn.data <- full.data %>%
   filter(Task == "WCN", TaskType == "immediate") %>%
-  dplyr::select(subID, Task, TaskType, TaskItem, Response, Accuracy, 
+  dplyr::select(LadlabID, Task, TaskType, TaskItem, Response, Accuracy, 
          Productivity, Productivity.tertiary, ProductivityStrict, ProductivityStrict.tertiary,
          prod.gradient, Age, AgeGroup, AgeMonths, IHC, FHC, DCE) %>%
   mutate(Response_num = as.numeric(as.character(Response)), 
@@ -115,39 +131,44 @@ validate() # ok
 
 # Add overall accuracy to the dataframe.
 wcn.accuracy <- wcn.data %>% 
-  group_by(subID) %>%
+  group_by(LadlabID) %>%
   mutate(wcnscore=sum(Accuracy)) %>%
-  dplyr::select(subID, wcnscore) %>%
+  dplyr::select(LadlabID, wcnscore) %>%
   unique()
 
-wcn.data <- left_join(wcn.data, wcn.accuracy, by="subID")
+wcn.data <- left_join(wcn.data, wcn.accuracy, by="LadlabID")
 
+##RMS: We don't need this anymore - everything is in wcn.data
 # + Code if WCN trial is Within / beyond IHC ----
 # first, get initial highest count for each kiddo
 # Make a lookup table with SID and initial highest count
-lookup <- full.data %>%
-  distinct(subID, IHC)
+# lookup <- full.data %>%
+#   distinct(LadlabID, IHC)
 
 # For each trial, check the number queried.
 # If number queried is above the child's initial highest count, marks that trial as beyond count range.
-determine_count_range <- function(df) {
-  tmp <- df
-  for (row in 1:nrow(tmp)) {
-    sub = as.character(tmp[row, "subID"])
-    count_range = as.numeric(as.character(subset(lookup, subID == sub)$IHC))
-    tmp[row, "IHC"] = as.numeric(as.character(count_range))
-    if (tmp[row, "TaskItem_num"] > count_range) {
-      tmp[row, "WithinOutsideIHC"] = "outside"
-    } else {
-      tmp[row, "WithinOutsideIHC"] = "within"
-    }
-  }
-  return(tmp)
-}
+# determine_count_range <- function(df) {
+#   tmp <- df
+#   for (row in 1:nrow(tmp)) {
+#     sub = as.character(tmp[row, "subID"])
+#     count_range = as.numeric(as.character(subset(lookup, subID == sub)$IHC))
+#     tmp[row, "IHC"] = as.numeric(as.character(count_range))
+#     if (tmp[row, "TaskItem_num"] > count_range) {
+#       tmp[row, "WithinOutsideIHC"] = "outside"
+#     } else {
+#       tmp[row, "WithinOutsideIHC"] = "within"
+#     }
+#   }
+#   return(tmp)
+# }
 
-# Run for wcn data
-wcn.data <- determine_count_range(wcn.data)
-summary(wcn.data$Accuracy_valid) # ok
+# # Run for wcn data
+# wcn.data <- determine_count_range(wcn.data)
+# summary(wcn.data$Accuracy_valid) # ok
+
+##RMS: This is replacing the code above
+wcn.data %<>%
+  mutate(WithinOutsideIHC = ifelse(TaskItem_num < IHC, "within", "outside"))
 
 # Filter out just the trials for analysis (without momentum)
 # And recode item type as decade transition or mid decade
@@ -172,14 +193,14 @@ data.full <- full.data %>% dplyr::select(-c("LadlabID", "DOT", "ExclusionGroup")
 data.hcerrors <- hc.errorscoded %>% dplyr::select(-c("LadlabID", "SortedID"))
 # Make dataframe for subject-level highest count measurements (without errors)
 data.hcunique <- full.data %>%
-  dplyr::distinct(subID, Gender, Age, AgeGroup, AgeMonths, HCReceivedSupport, IHC, DCE, FHC, Productivity, Productivity.tertiary, ProductivityStrict, ProductivityStrict.tertiary)
+  dplyr::distinct(LadlabID, Gender, Age, AgeGroup, AgeMonths, HCReceivedSupport, IHC, DCE, FHC, Productivity, Productivity.tertiary, ProductivityStrict, ProductivityStrict.tertiary)
 # Make long dataframe for item-level Next Number data
 data.wcn.long <- data.wcn %>%
-  dplyr::select(c(subID, IHC, FHC, Age, 
+  dplyr::select(c(LadlabID, IHC, FHC, Age, 
                   Productivity, Productivity.tertiary, ProductivityStrict, ProductivityStrict.tertiary,
                   TaskItem_num, TaskItem_type, Accuracy, WithinOutsideIHC)) %>%
-  mutate(IHC = as.integer(IHC), subID=as.factor(subID), TaskItem_type=as.factor(TaskItem_type))
-data.wcn.wide <- data.wcn.long %>% group_by(subID, IHC, Age, Productivity, Productivity.tertiary, ProductivityStrict, ProductivityStrict.tertiary) %>%
+  mutate(IHC = as.integer(IHC), LadlabID=as.factor(LadlabID), TaskItem_type=as.factor(TaskItem_type))
+data.wcn.wide <- data.wcn.long %>% group_by(LadlabID, IHC, Age, Productivity, Productivity.tertiary, ProductivityStrict, ProductivityStrict.tertiary) %>%
   summarise(score=sum(Accuracy), perc=mean(Accuracy)) %>% ungroup()
 # Export
 save(data.full, data.hcunique, data.hcerrors, 
